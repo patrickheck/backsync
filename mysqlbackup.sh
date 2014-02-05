4267fd1ea329d755616edee6c6e139db39531daa
Patrick Heck <patrick@patrickheck.de>
2014-02-05 12:54:01 +0100

    add mysqlbackup.sh

---
+++ mysqlbackup.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+# written 2014 by Patrick Heck (heckp@uni-trier.de)
+# this performs a MYSQL backup
+# (Apple Time Machine Style)
+
+echo "Starting database backup"
+DATE=`date "+%Y-%m-%dT%H-%M-%S"`
+
+MAX_BACKUP_DAYS=45
+# This is the day who's backups will expire today
+EXPIRING_DATE=`date "+%Y-%m-%d" -d "${MAX_BACKUP_DAYS} days ago"` 
+
+# always exit if a command fails 
+set -o errexit
+
+# Source configuration file
+if [ -f $CONFIG_FILE ]; then
+    . $CONFIG_FILE
+fi
+
+echo "Backing up MySQL Databases" 
+for DATABASE in $MYSQL_DATABASES
+do
+	DBBACKUP_DIR="${MYSQL_BACKUP_DIR}/${DATABASE}"
+	mkdir -p "$DBBACKUP_DIR"
+	NEW="${DBBACKUP_DIR}/${DATABASE}_dump_${DATE}.sql.bz"
+	CURRENT="${DBBACKUP_DIR}/${DATABASE}_dump_current"
+	echo "Backing up $DATABASE to ${DBBACKUP_DIR}"
+	echo "File: $NEW"
+	/usr/bin/mysqldump --skip-comments --quick -u $MYSQL_USERNAME --password=$MYSQL_PASSWORD $DATABASE | bzip2 > "$NEW"
+	# if the files are the same make new file hardlink to old one
+	if diff "$NEW" "$CURRENT"
+	then
+		echo "Files are the same. Creating link to last version"
+		ln -fv `readlink "$CURRENT"` "$NEW"
+		touch "$NEW"
+	else
+		echo "Files differ."
+	fi
+	rm -rf "$CURRENT"
+	ln -s "$NEW" "$CURRENT"
+        echo "Deleting old Database Backup Files"
+        find "${DBBACKUP_DIR}" -name '*bz' -mtime +$MAX_BACKUP_DAYS -exec rm -fv {} \;
+
+done
+echo "done"

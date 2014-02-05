22c0c8f7188e82bc0b77c5adc009338c44dd41a5
Patrick Heck <patrick@patrickheck.de>
2014-02-05 12:46:34 +0100

    add rsyncbackup.sh again

---
+++ rsyncbackup.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+# written 2013 by Patrick Heck (heckp@uni-trier.de)
+# this performs a remote backup with hard links to older versions
+# (Apple Time Machine Style)
+
+BASEDIR=$(dirname $0)
+echo $(dirname $0)
+
+echo "Starting incremental rsync backup"
+DATE=`date "+%Y-%m-%dT%H-%M-%S"`
+
+CONFIG_FILE="${BASEDIR}/backup.conf"
+
+echo "loading configuration from ${CONFIG_FILE}"
+
+# Source configuration file
+if [ -f $CONFIG_FILE ]; then
+    . $CONFIG_FILE
+fi
+
+
+
+# read the dirs to be backed up from here
+RSYNCDIRS="${BASEDIR}/rsyncdirs"
+
+# files and directories to be excluded
+RSYNCRULES="${BASEDIR}/rsyncrules"
+
+MAX_BACKUP_DAYS=45
+# This is the day which's backups will expire today
+EXPIRING_DATE=`date "+%Y-%m-%d" -d "${MAX_BACKUP_DAYS} days ago"` 
+
+# always exit if a command fails 
+set -o errexit
+
+
+echo "Backing up Files"
+echo "Target is \"${BACKUP_SERVER}:${BACKUP_DIR}/Backup-${DATE}\""
+
+rsync -aPrv --delete --delete-excluded \
+	  --exclude-from="${RSYNCRULES}" \
+	  --files-from="${RSYNCDIRS}" \
+	  --copy-links \
+	  --link-dest="../current" \
+	  / \
+	  "${BACKUP_SERVER}:${BACKUP_DIR}/Backup-${DATE}.inprogress"
+ssh ${BACKUP_SERVER} \
+"mv \"${BACKUP_DIR}/Backup-${DATE}.inprogress\" \"${BACKUP_DIR}/Backup-${DATE}\" \
+&& rm -rf \"${BACKUP_DIR}/current\" \
+&& ln -s \"${BACKUP_DIR}/Backup-${DATE}\" \"${BACKUP_DIR}/current\" "
+echo "Copying of data complete"
+
+echo "Deleting data older than $MAX_BACKUP_DAYS days"
+echo "Expiring Backup is ${BACKUP_DIR}/Backup-${EXPIRING_DATE}*"
+# ssh ${BACKUP_SERVER} \
+# "find \"${BACKUP_DIR}\" -maxdepth 1 -type d -mtime +$MAX_BACKUP_DAYS -exec rm -rf  {} \\; \
+# ; find \"${BACKUP_DIR}\" -maxdepth 1 -type d -name '*.inprogress' -exec rm -rf  {} \\; "
+ssh ${BACKUP_SERVER} \
+"rm -rf \"${BACKUP_DIR}/Backup-${EXPIRING_DATE}\"* \
+; rm -rf \"${BACKUP_DIR}\"/*.inprogress "
+echo "Backup Complete"
+
